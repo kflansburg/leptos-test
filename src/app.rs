fd3a0dd60bf54a9a1d5c63bfae92b7262c2d7b6f
Kevin Flansburg <kflansburg@cloudflare.com>
2024-09-23 15:40:05 -0400

    change

--- src/app.rs
+++ src/app.rs
@@ -5,7 +5,7 @@ use crate::components::show_data_from_api::ShowDataFromApi;
 #[component]
 pub fn App() -> impl IntoView {
     view! {
-        <h1>"Hello world!"</h1>
+        <h1>"Hello Workers CI!"</h1>
         <ShowDataFromApi />
     }
 }
